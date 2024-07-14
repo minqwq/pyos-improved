e771d4687c0207a0ca697e8582056d696b9f42e7
minqwq <minqwq723897@outlook.com>
2024-07-14 09:55:14 +0800

    更新 installdepend.sh

--- installdepend.sh
+++ installdepend.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+# Texts
+echo "Installing packages..."
+
 # Function to install packages based on distribution
 install_packages() {
     distro=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '"')
 
     case $distro in
         arch)
+            echo "Detected you are Arch Linux, using pacman to install packages..."
             sudo pacman -Sy --noconfirm ranger python python-pip git mpg123 libopenmpt sidplayfp wildmidi
             ;;
         ubuntu|debian)
+            echo "Detected you are Ubuntu or Debian, Updating your system and install packages..."
             sudo apt update
             sudo apt install -y ranger python3 python3-pip git mpg123 libopenmpt-dev sidplayfp wildmidi
             ;;
         fedora)
+            echo "Detected you are Fedora, using dnf to install packages..."
             sudo dnf install -y ranger python3 python3-pip git mpg123 libopenmpt-devel sidplayfp wildmidi
             ;;
         opensuse-leap|opensuse-tumbleweed)
+            echo "Detected you are Opensuse, using zypper to install packages..."
             sudo zypper install -y ranger python3 python3-pip git mpg123 libopenmpt-devel sidplayfp wildmidi
             ;;
         *)
-            echo "Unsupported distribution: $distro"
+            echo "Unsupported distribution(Are you using Windows?): $distro"
             exit 1
             ;;
     esac
@@ -27,7 +34,7 @@ install_packages() {
 
 # Function to install pip packages
 install_pip_packages() {
-    sudo pip3 install psutil Pillow matplotlib colorama datetime
+    sudo pip3 install psutil colorama datetime
 }
 
 # Main execution starts here
@@ -35,3 +42,4 @@ install_packages
 install_pip_packages
 
 echo "Installation complete ~\(≧▽≦)/~"
+echo "Type 'python3 pyosimproved.py' to start system..."
