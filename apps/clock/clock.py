cddb7f99d81ef8611d7ee72651aa65e1e1feb082
minqwq <minqwq723897@outlook.com>
2025-08-10 02:36:12 +0800

    clock bug fixed, whatever im gonna to go to debtrixie

--- apps/clock/clock.py
+++ apps/clock/clock.py
@@ -1,7 +1,5 @@
 import tqdm
 import time
-import pygame
-pygame.mixer.init()
 print("1:timer\n2:timer(rev)\n3:current time\n4:exit")
 choice = input("Option?: ")
 if choice == "1":
@@ -17,9 +15,5 @@ elif choice == "2":
         print("Range is none.")
     else:
         for revtimer in tqdm.tqdm(range(int(setRevTimerEndTime))):
-            pygame.mixer.music.load("./audio/se/info.mp3")
-            pygame.mixer.music.play()
             time.sleep(1)
-        pygame.mixer.music.load("./audio/se/success_long.mp3")
-        pygame.mixer.music.play()
         input("Press any key to exit")
